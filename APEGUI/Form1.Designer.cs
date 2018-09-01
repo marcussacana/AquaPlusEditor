755389944bfb86e44983a8f5cf7beb59ea5ec5c3
= <ddtank.marcus@gmail.com>
2018-09-01 06:09:30 -0300

    ..

--- APEGUI/Form1.Designer.cs
+++ APEGUI/Form1.Designer.cs
@@ -30,12 +30,12 @@
             this.pakToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.extractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.repackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.fntToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.texToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.decodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.encodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fntToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,19 +48,19 @@
             this.texToolStripMenuItem,
             this.fntToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 136);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 114);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
@@ -70,7 +70,7 @@
             this.extractToolStripMenuItem,
             this.repackToolStripMenuItem});
             this.pakToolStripMenuItem.Name = "pakToolStripMenuItem";
-            this.pakToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pakToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.pakToolStripMenuItem.Text = "Pak";
             // 
             // extractToolStripMenuItem
@@ -87,10 +87,33 @@
             this.repackToolStripMenuItem.Text = "Repack";
             this.repackToolStripMenuItem.Click += new System.EventHandler(this.repackToolStripMenuItem_Click);
             // 
+            // texToolStripMenuItem
+            // 
+            this.texToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.decodeToolStripMenuItem,
+            this.encodeToolStripMenuItem});
+            this.texToolStripMenuItem.Name = "texToolStripMenuItem";
+            this.texToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.texToolStripMenuItem.Text = "Tex";
+            // 
+            // decodeToolStripMenuItem
+            // 
+            this.decodeToolStripMenuItem.Name = "decodeToolStripMenuItem";
+            this.decodeToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.decodeToolStripMenuItem.Text = "Decode";
+            this.decodeToolStripMenuItem.Click += new System.EventHandler(this.decodeToolStripMenuItem_Click);
+            // 
+            // encodeToolStripMenuItem
+            // 
+            this.encodeToolStripMenuItem.Name = "encodeToolStripMenuItem";
+            this.encodeToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.encodeToolStripMenuItem.Text = "Encode";
+            this.encodeToolStripMenuItem.Click += new System.EventHandler(this.encodeToolStripMenuItem_Click);
+            // 
             // fntToolStripMenuItem
             // 
             this.fntToolStripMenuItem.Name = "fntToolStripMenuItem";
-            this.fntToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fntToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.fntToolStripMenuItem.Text = "Fnt";
             this.fntToolStripMenuItem.Click += new System.EventHandler(this.fntToolStripMenuItem_Click);
             // 
@@ -102,7 +125,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(290, 238);
+            this.listBox1.Size = new System.Drawing.Size(452, 342);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -110,40 +133,17 @@
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 256);
+            this.textBox1.Location = new System.Drawing.Point(12, 370);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(290, 20);
+            this.textBox1.Size = new System.Drawing.Size(452, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
-            // 
-            // texToolStripMenuItem
-            // 
-            this.texToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.decodeToolStripMenuItem,
-            this.encodeToolStripMenuItem});
-            this.texToolStripMenuItem.Name = "texToolStripMenuItem";
-            this.texToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.texToolStripMenuItem.Text = "Tex";
-            // 
-            // decodeToolStripMenuItem
-            // 
-            this.decodeToolStripMenuItem.Name = "decodeToolStripMenuItem";
-            this.decodeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.decodeToolStripMenuItem.Text = "Decode";
-            this.decodeToolStripMenuItem.Click += new System.EventHandler(this.decodeToolStripMenuItem_Click);
-            // 
-            // encodeToolStripMenuItem
-            // 
-            this.encodeToolStripMenuItem.Name = "encodeToolStripMenuItem";
-            this.encodeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.encodeToolStripMenuItem.Text = "Encode";
-            this.encodeToolStripMenuItem.Click += new System.EventHandler(this.encodeToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 290);
+            this.ClientSize = new System.Drawing.Size(476, 404);
             this.ContextMenuStrip = this.contextMenuStrip1;
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
